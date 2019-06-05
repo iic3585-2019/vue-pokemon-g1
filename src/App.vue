e2c7fbdcae34bb6dd376f2a6ea089bc9a957ca4c
MainScientist <fipezoa@uc.cl>
2019-06-04 20:08:48 -0400

    add vue-material

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <md-progress-spinner md-mode="indeterminate"></md-progress-spinner>
   </div>
 </template>
 
